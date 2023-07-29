913060c2b8394a7a5917672b011d963aa491bb9b
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-07-29 21:51:16 +0700

    modify logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,9 +14,9 @@
         </div>
       </div>
       @error('email')
-        <span class="invalid-feedback" role="alert">
-          <strong>{{ $message }}</strong>
-        </span>
+      <span class="invalid-feedback" role="alert">
+        <strong>{{ $message }}</strong>
+      </span>
       @enderror
     </div>
     <div class="input-group mb-3">
@@ -27,9 +27,9 @@
         </div>
       </div>
       @error('password')
-        <span class="invalid-feedback" role="alert">
-          <strong>{{ $message }}</strong>
-        </span>
+      <span class="invalid-feedback" role="alert">
+        <strong>{{ $message }}</strong>
+      </span>
       @enderror
     </div>
     <div class="row mb-1">
@@ -43,7 +43,7 @@
       </div>
       <!-- /.col -->
       <div class="col-5">
-        <button type="submit" id="btn-login"class="btn btn-primary btn-block" disabled>{{ __('Login') }} &nbsp; <i class="nav-icon fas fa-sign-in-alt"></i></button>
+        <button type="submit" id="btn-login" class="btn btn-primary btn-block" disabled>{{ __('Login') }} &nbsp; <i class="nav-icon fas fa-sign-in-alt"></i></button>
       </div>
       <!-- /.col -->
     </div>
@@ -51,9 +51,9 @@
 
   <p class="mb-1">
     @if (Route::has('password.request'))
-      <a class="text-center" href="{{ route('password.request') }}">
-        {{ __('Lupa Password?') }}
-      </a>
+    <a class="text-center" href="{{ route('password.request') }}">
+      {{ __('Lupa Password?') }}
+    </a>
     @endif
   </p>
   <p class="mb-0">
@@ -62,81 +62,79 @@
 </div>
 @endsection
 @section('script')
-  <script>
-    $("#email").keyup(function(){
-        var email = $("#email").val();
+<script>
+  $("#email").keyup(function() {
+    var email = $("#email").val();
 
-        if (email.length >= 5){
-          $.ajax({
-              type:"GET",
-              data: {
-                  email : email
-              },
-              dataType:"JSON",
-              url:"{{ url('/login/cek_email/json') }}",
-              success:function(data){
-                if (data.success) {
-                  $("#email").removeClass("is-invalid");
-                  $("#email").addClass("is-valid");
-                  $("#password").val('');
-                  $("#password").removeAttr("disabled", "disabled");
-                } else {
-                  $("#email").removeClass("is-valid");
-                  $("#email").addClass("is-invalid");
-                  $("#password").val('');
-                  $("#password").attr("disabled", "disabled");
-                  $("#remember").attr("disabled", "disabled");
-                  $("#btn-login").attr("disabled", "disabled");
-                }
-              },
-              error:function(){
-              }
-          });
-        } else {
-          $("#email").removeClass("is-valid");
-          $("#email").removeClass("is-invalid");
-          $("#password").val('');
-          $("#password").attr("disabled", "disabled");
-          $("#remember").attr("disabled", "disabled");
-          $("#btn-login").attr("disabled", "disabled");
-        }
-    });
+    if (email.length >= 5) {
+      $.ajax({
+        type: "GET",
+        data: {
+          email: email
+        },
+        dataType: "JSON",
+        url: "{{ url('/login/cek_email/json') }}",
+        success: function(data) {
+          if (data.success) {
+            $("#email").removeClass("is-invalid");
+            $("#email").addClass("is-valid");
+            $("#password").val('');
+            $("#password").removeAttr("disabled", "disabled");
+          } else {
+            $("#email").removeClass("is-valid");
+            $("#email").addClass("is-invalid");
+            $("#password").val('');
+            $("#password").attr("disabled", "disabled");
+            $("#remember").attr("disabled", "disabled");
+            $("#btn-login").attr("disabled", "disabled");
+          }
+        },
+        error: function() {}
+      });
+    } else {
+      $("#email").removeClass("is-valid");
+      $("#email").removeClass("is-invalid");
+      $("#password").val('');
+      $("#password").attr("disabled", "disabled");
+      $("#remember").attr("disabled", "disabled");
+      $("#btn-login").attr("disabled", "disabled");
+    }
+  });
 
-    $("#password").keyup(function(){
-        var email = $("#email").val();
-        var password = $("#password").val();
+  $("#password").keyup(function() {
+    var email = $("#email").val();
+    var password = $("#password").val();
 
-        if (password.length >= 8){
-          $.ajax({
-              type:"GET",
-              data: {
-                  email : email,
-                  password : password
-              },
-              dataType:"JSON",
-              url:"{{ url('/login/cek_password/json') }}",
-              success:function(data){
-                if (data.success) {
-                  $("#password").removeClass("is-invalid");
-                  $("#password").addClass("is-valid");
-                  $("#remember").removeAttr("disabled", "disabled");
-                  $("#btn-login").removeAttr("disabled", "disabled");
-                } else {
-                  $("#password").removeClass("is-valid");
-                  $("#password").addClass("is-invalid");
-                  $("#remember").attr("disabled", "disabled");
-                  $("#btn-login").attr("disabled", "disabled");
-                }
-              },
-              error:function(){
-              }
-          });
-        } else {
-          $("#password").removeClass("is-valid");
-          $("#password").removeClass("is-invalid");
-          $("#remember").attr("disabled", "disabled");
-          $("#btn-login").attr("disabled", "disabled");
-        }
-    });
-  </script>
+    if (password.length >= 8) {
+      $.ajax({
+        type: "GET",
+        data: {
+          email: email,
+          password: password
+        },
+        dataType: "JSON",
+        url: "{{ url('/login/cek_password/json') }}",
+        success: function(data) {
+          if (data.success) {
+            $("#password").removeClass("is-invalid");
+            $("#password").addClass("is-valid");
+            $("#remember").removeAttr("disabled", "disabled");
+            $("#btn-login").removeAttr("disabled", "disabled");
+          } else {
+            $("#password").removeClass("is-valid");
+            $("#password").addClass("is-invalid");
+            $("#remember").attr("disabled", "disabled");
+            $("#btn-login").attr("disabled", "disabled");
+          }
+        },
+        error: function() {}
+      });
+    } else {
+      $("#password").removeClass("is-valid");
+      $("#password").removeClass("is-invalid");
+      $("#remember").attr("disabled", "disabled");
+      $("#btn-login").attr("disabled", "disabled");
+    }
+  });
+</script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,8 @@
-
 <!DOCTYPE html>
 <html>
+
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>Sistem Informasi Akademik Sekolah</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
@@ -11,10 +11,12 @@
   <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
   <link rel="shrotcut icon" href="{{ asset('img/favicon.ico') }}">
 </head>
+
 <body class="hold-transition login-page" style="background-image: url('{{ asset("img/background-login.jpg") }}'); background-size: cover; background-attachment: fixed;">
   <div class="login-box">
     <div class="login-logo">
-      <h1 style="font-family: cursive; font-size: 46px; color: #ffff; text-align: center;">SMPIT INSAN MADANI</h1>
+      <img src="{{ asset('img/logo.png') }}" alt="Logo" width="150" height="150">
+      <h1 style="font-family: cursive; font-size: 26px; color: #ffff; text-align: center;">SMPIT INSAN MADANI</h1>
       {{-- <img src="{{ asset('img/logosiakad.png') }}" width="100%" alt=""> --}}
     </div>
 
@@ -33,17 +35,17 @@
     </footer>
   </div>
 
-<!-- jQuery -->
-<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('plugins/toastr/toastr.min.js') }}"></script>
-<!-- page script -->
-<script>
-  $(document).ready(function(){
-      $('#role').change(function(){
-          var kel = $('#role option:selected').val();
-          if (kel == "Guru") {
-            $("#noId").addClass("mb-3");
-            $("#noId").html(`
+  <!-- jQuery -->
+  <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('plugins/toastr/toastr.min.js') }}"></script>
+  <!-- page script -->
+  <script>
+    $(document).ready(function() {
+      $('#role').change(function() {
+        var kel = $('#role option:selected').val();
+        if (kel == "Guru") {
+          $("#noId").addClass("mb-3");
+          $("#noId").html(`
               <input id="nomer" type="text" maxlength="5" onkeypress="return inputAngka(event)" placeholder="No Id Card" class="form-control @error('nomer') is-invalid @enderror" name="nomer" autocomplete="nomer">
               <div class="input-group-append">
                 <div class="input-group-text">
@@ -51,16 +53,16 @@
                 </div>
               </div>
               `);
-            $("#pesan").html(`
+          $("#pesan").html(`
               @error('nomer')
                 <span class="invalid-feedback" role="alert">
                   <strong>{{ $message }}</strong>
                 </span>
               @enderror
             `);
-          } else if(kel == "Siswa") {
-            $("#noId").addClass("mb-3");
-            $("#noId").html(`
+        } else if (kel == "Siswa") {
+          $("#noId").addClass("mb-3");
+          $("#noId").html(`
               <input id="nomer" type="text" placeholder="No Induk Siswa" class="form-control" name="nomer" autocomplete="nomer">
               <div class="input-group-append">
                 <div class="input-group-text">
@@ -68,59 +70,61 @@
                 </div>
               </div>
             `);
-            $("#pesan").html(`
+          $("#pesan").html(`
               @error('nomer')
                 <span class="invalid-feedback" role="alert">
                   <strong>{{ $message }}</strong>
                 </span>
               @enderror
             `);
-          } else {
-            $('#noId').removeClass("mb-3");
-            $('#noId').html('');
-          }
+        } else {
+          $('#noId').removeClass("mb-3");
+          $('#noId').html('');
+        }
       });
-  });
-  function inputAngka(e) {
-    var charCode = (e.which) ? e.which : event.keyCode
-    if (charCode > 31 && (charCode < 48 || charCode > 57)){
-      return false;
-    }
-    return true;
-  }
-</script>
-@yield('script')
+    });
 
-@error('id_card')
+    function inputAngka(e) {
+      var charCode = (e.which) ? e.which : event.keyCode
+      if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+      }
+      return true;
+    }
+  </script>
+  @yield('script')
+
+  @error('id_card')
   <script>
     toastr.error("Maaf User ini tidak terdaftar sebagai Guru!");
   </script>
-@enderror
-@error('guru')
+  @enderror
+  @error('guru')
   <script>
     toastr.error("Maaf Guru ini sudah terdaftar sebagai User!");
   </script>
-@enderror
-@error('no_induk')
+  @enderror
+  @error('no_induk')
   <script>
     toastr.error("Maaf User ini tidak terdaftar sebagai Siswa!");
   </script>
-@enderror
-@error('siswa')
+  @enderror
+  @error('siswa')
   <script>
     toastr.error("Maaf Siswa ini sudah terdaftar sebagai User!");
   </script>
-@enderror
-@if (session('status'))
+  @enderror
+  @if (session('status'))
   <script>
     toastr.success("{{ Session('success') }}");
   </script>
-@endif
-@if (Session::has('error'))
-    <script>
-        toastr.error("{{ Session('error') }}");
-    </script>
-@endif
+  @endif
+  @if (Session::has('error'))
+  <script>
+    toastr.error("{{ Session('error') }}");
+  </script>
+  @endif
 
 </body>
+
 </html>
